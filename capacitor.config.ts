2fcd5af03f1a035a1f62f56eced8d9a922963d6a
Mohammad Khalili <khalili@Khalilis-Mac.local>
2022-11-13 17:06:08 +0330

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SmartWorld',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
